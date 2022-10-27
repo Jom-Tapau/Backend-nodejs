d46c317b6c1eaa57ee0cb48307f1f1c5210c3511
GMShimon <simon.rosedale99@gmail.com>
2022-10-27 15:23:52 +0800

    cors initialize

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express();
+const {MongoClient} = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const port = 5000;
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('Hello World');
