7741028a1f5ffacce215787d600a5a8d647c4446
GMShimon <simon.rosedale99@gmail.com>
2022-11-04 12:40:45 +0800

    mongodb connection

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient,ServerApiVersion} = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -8,10 +8,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=>{
-    res.send('Hello World');
-});
+//user=dbuser2;
+//password=AnOyXN8v59bGpYJE
 
+const uri = 'mongodb+srv://dbuser2:AnOyXN8v59bGpYJE@cluster0.in8lp.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+})
+
+async function run (){
+    try{
+        await client.connect();
+        const foodCollection = client.db("Jom-tapau").collection("food");
+
+        app.get('/', (req, res)=>{
+            res.send('Hello World');
+        });
+
+    }finally{
+    }
+}
+run().catch(console.dir);
 app.listen(port,()=>{
     console.log('Listening on port',port);
 })
