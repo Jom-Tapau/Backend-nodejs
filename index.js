26d138e724f662dc236d9683951431a8c064af45
GMShimon <simon.rosedale99@gmail.com>
2022-11-04 23:30:01 +0800

    mongodb connection changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 
-const uri = `mongodb+srv://${user}:${password}@cluster0.in8lp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${user}:${password}@cluster0.xpxsbcb.mongodb.net/?retryWrites=true&w=majority"`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 async function run (){
     try{
         await client.connect();
-        const foodCollection = client.db("Jom-tapau").collection("food");
+        // const foodCollection = client.db("Jom-tapau").collection("food");
 
         app.get('/', (req, res)=>{
             res.send('Hello World');
