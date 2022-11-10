90311716a203fce3f138c58656cf52a709fc70d3
GMShimon <simon.rosedale99@gmail.com>
2022-11-10 14:58:37 +0800

    todo list added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const port = 5000 || PROCESS.ENV.PORT
 
-//middleware 
+//middleware
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -22,27 +22,27 @@ const client = new MongoClient(uri, {
 async function run () {
   try {
     await client.connect()
-    const foodCollection = client.db('Jom-tapau').collection('foodCollection');//collection of food items
+    const foodCollection = client.db('Jom-tapau').collection('foodCollection') //collection of food items
 
-    const userCollection = client.db('Jom-tapau').collection('userCollection'); //collection of user items
+    const userCollection = client.db('Jom-tapau').collection('userCollection') //collection of user items
 
     //get data from the food collection
-    app.get('/food',async(req,res)=>{
-        const query ={};
-        const cursor = await foodCollection.find(query);
-        const result = await cursor.toArray();
+    app.get('/food', async (req, res) => {
+      const query = {}
+      const cursor = await foodCollection.find(query)
+      const result = await cursor.toArray()
 
-        res.send(result);
+      res.send(result)
     })
 
-    // get food by id 
-    app.get('/food/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query ={_id:ObjectId(id)}
-        const result = await foodCollection.findOne(query);
+    // get food by id
+    app.get('/food/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await foodCollection.findOne(query)
 
-        res.send(result);
-        console.log(id);
+      res.send(result)
+      console.log(id)
     })
 
     // post a food on the server
@@ -55,19 +55,26 @@ async function run () {
 
     // post user information to mongodb
     app.post('/user', async (req, res) => {
-      const newUser = req.body;
-      console.log('Adding new user', newUser);
-      const result = await userCollection.insertOne(newUser);
-      res.send(result);
+      const newUser = req.body
+      console.log('Adding new user', newUser)
+      const result = await userCollection.insertOne(newUser)
+      res.send(result)
     })
 
     //get user by id
     app.get('/user/:id', async (req, res) => {
-      const id = req.params.id;
-      const query ={_id:ObjectId(id)};
-      const result = await userCollection.findOne(query);
-      res.send(result);
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await userCollection.findOne(query)
+      res.send(result)
     })
+
+    //TODO: delete user by id
+    //TODO: post order to order list
+    //TODO: get all order list
+    //TODO: get a specific users' order list
+    //TODO: update an order when rider accept the order and complete the order
+    //TODO: delete a specific food by id
   } finally {
   }
 }
