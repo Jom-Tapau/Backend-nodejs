8bc0445c6b2fb9c9012a413aecae4405e4c0556a
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-01-14 19:39:58 +0800

    update prfile

--- index.js
+++ index.js
@@ -156,6 +156,10 @@ async function run () {
     })
     app.get('/users/:id', async(req,res)=>{
       const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const user = await userCollection.findOne(query);
+      res.send(user);
+
     })
     //update food item
     app.put('/food/:id',async(req,res)=>{
